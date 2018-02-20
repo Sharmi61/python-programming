52f44faddfa455d3be4f16aed8f41d8b65f26941
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-02-20 11:46:25 +0530

    Add files via upload

---
+++ beginner level/arith.py
@@ -0,0 +1,30 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      user
+#
+# Created:     20/02/2018
+# Copyright:   (c) user 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+     a=1
+
+     b=2
+     c=3
+     d=4
+     e=5
+     f=6
+     g=7
+     h=8
+     i=9
+     j=0
+     k=a+b;print k;
+     l=d-c-j;print l;
+     m=e*f*g;print m;
+     n=i/h;print n;
+
+if __name__ == '__main__':
+    main()
