0b1750c79926087732e554ef664d7b07666963ce
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-02-20 15:28:37 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      user
+#
+# Created:     20-02-2018
+# Copyright:   (c) user 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    n = 0
+    if False:
+     n += 2;
+     print n;
+    n += 3
+    if True:
+     n += 4;
+     print n;
+
+if __name__ == '__main__':
+    main()
