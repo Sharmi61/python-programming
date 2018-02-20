32871b47cb3c4c4eef72e5bf07ccfbdd01274786
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-02-20 15:59:47 +0530

    Add files via upload

---
+++ beginner level/voting_age_eligiblity(ifelse).py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      user
+#
+# Created:     20-02-2018
+# Copyright:   (c) user 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    i=int(input())
+    if(i>=18):
+        print("eligible for voting")
+    else:
+        print("not eligible")
+
+
+if __name__ == '__main__':
+    main()
