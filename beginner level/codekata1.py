aa54afa0c0f59e40b2e51b602089388a36fb9466
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-16 11:37:33 +0530

    Update codekata1.py

--- beginner level/codekata1.py
+++ beginner level/codekata1.py
@@ -1,13 +1,14 @@
 def main():
   i=int(raw_input("enter number between 0 to 100000"))
-  if i<0 and i>-100000:
-  	print("\nit is negative")
-  if i>0 and i<100000:
-  	print("\nit is positive")
+  if i<0 and i>-100001:
+  	print("\nNegative")
+  if i>0 and i<100001:
+  	print("\nPositive")
   if i==0:
-  	print("\nit is zero")
-  else:
-  	print("\nnumber not lies in between -100000 to 100000")
+  	print("\nZero")
+
 main()  	
   	
   
+    
+      
