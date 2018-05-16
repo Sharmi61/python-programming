19053d140ef1b4c86db2166021527172f319e19d
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-16 14:09:01 +0530

    Create codekataset2.py

---
+++ beginner level/codekataset2.py
@@ -0,0 +1,10 @@
+def main():
+	
+  a=int(raw_input())
+  if(a>=0 and a<=10000):
+       	
+           if(a%2==0):
+           	print("the given number is even",a)
+           else:
+           	print("the given number is odd",a)
+main()
