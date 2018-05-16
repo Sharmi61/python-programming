95641ca4fce51b8abda39720b7f94e8094eff325
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-16 15:42:48 +0530

    Add files via upload

---
+++ beginner level/codekata3.py
@@ -0,0 +1,21 @@
+s=raw_input()
+
+l=['a','e','i','o','u']
+
+r=1
+
+for x in l:
+	
+             if s == x:
+		
+             r=0
+		
+            break
+
+if r==0:
+	
+  print "vowel"
+
+elif r!=0 :
+	
+ print "constant"
