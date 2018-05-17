7e7dd667b4baa72ec2beed6f470714e88bd10890
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-17 09:26:30 +0530

    Add files via upload

---
+++ beginner level/codekata212.py
@@ -0,0 +1,16 @@
+def main():
+ 
+ a=int(raw_input())
+ 
+ if(a>=0 and a<=100000):
+         
+              if(a%2==0):
+          		
+                        print("Even")
+         
+              else :
+            	
+                       print("Odd")
+            
+
+main()
