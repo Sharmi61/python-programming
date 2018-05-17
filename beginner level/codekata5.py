e846b37f7ba19d0906d2aeb93a93fbdd3020ae0b
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-17 10:16:21 +0530

    Add files via upload

---
+++ beginner level/codekata5.py
@@ -0,0 +1,14 @@
+a,b,c=[int(x) for x in raw_input().split()]
+
+if a>=b and a>=c :
+	
+                      print a
+
+elif b>=a and b>=c:
+	
+                      print b
+
+else:
+	   
+                      print c
+	
