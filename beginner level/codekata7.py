19cae61cc3a9b98a3a90c2c037e8fc5da7f49343
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-19 06:07:56 -0700

    Add files via upload

---
+++ beginner level/codekata7.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     19-05-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+r=int(raw_input())
+for i in range(r) :
+	print 'Hello'
