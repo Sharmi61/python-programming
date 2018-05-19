7a6c2091603fd9527735e3b561c684f17be5cac3
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-19 06:23:31 -0700

    Add files via upload

---
+++ beginner level/codekata8.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     19-05-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+r=int(raw_input())
+r=r+1
+sum=0
+for i in range(r) :
+    sum=sum+i
+print sum
