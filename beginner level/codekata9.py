483451c3b6e24b5a6be565667d9316acaf748c06
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-20 05:26:01 -0700

    Add files via upload

---
+++ beginner level/codekata9.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     19-05-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#------------------------------------------------------------------------------
+a,b=raw_input().split()
+a= int(a)
+b= int(b)
+l=[int(x) for x in range(1,a+1)]
+r=0
+i=0
+for i in l[0:b ]:
+    r=r+i
+print r
+
