ca11242a587e94f6bdc487c45d58025fbe5e4c62
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-22 07:51:59 -0700

    Add files via upload

---
+++ beginner level/codekata14.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     22-05-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a,b=raw_input().split()
+a=int(a)
+b=int(b)
+l1=[]
+l=[int(x) for x in range(a+1,b)]
+for i in range(len(l)) :
+    if l[i]%2==1:
+        print l[i],
