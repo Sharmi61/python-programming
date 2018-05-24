6ecd31dee97fd0a62fbd6401a066db0e3ca53977
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-23 23:48:05 -0700

    Add files via upload

---
+++ player level/ckp3.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     23-05-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#------------------------------------------------------------------------------
+r=str(raw_input())
+l=[x for x in r]
+i=0
+l1=[]
+for i in range(len(l)-1,-1,-1):
+    l1.append(l[i])
+print ''.join(l1)
