ee7af5849b637a104b1bc1b4ab470d7a5fdd72a7
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-24 00:16:45 -0700

    Add files via upload

---
+++ player level/ckp4.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     24-05-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+r=str(raw_input())
+l=[x for x in r]
+l.append('.')
+print ''.join(l)
