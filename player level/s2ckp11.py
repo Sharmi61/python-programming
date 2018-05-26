d1716c79c0b0f0bc5c9fd69722a360a96e90800a
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-05-26 09:09:24 -0700

    Add files via upload

---
+++ player level/s2ckp11.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     26-05-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+r=str(raw_input())
+if r=="Sunday" and r=="sunday":
+    print 'yes'
+else :
+    print 'no'
