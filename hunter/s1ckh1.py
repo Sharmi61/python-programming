86ed3fe7c96fab404df9abe968a1c80adcb04676
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-06-19 08:30:54 -0700

    Update s1ckh1.py

--- hunter/s1ckh1.py
+++ hunter/s1ckh1.py
@@ -10,7 +10,6 @@
 #-------------------------------------------------------------------------------
 n=int(raw_input())
 l=[int(x) for x in raw_input().split()]
-print l
 for x in range(len(l)-1,0,-1):
     for i in range(x):
         if l[i]>l[i+1] :
