1180e39631fc5312ddcca4f1dfd829126f2866fd
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-07-15 17:09:30 +0530

    Add files via upload

---
+++ beginner level/codekata34sort.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     15-07-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+n=int(raw_input())
+l=[int(x) for x in raw_input().split()]
+l.sort()
+print l
