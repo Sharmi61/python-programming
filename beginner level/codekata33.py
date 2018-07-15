da669b447ee7be44bf2e760bd5ebc69072d7bc23
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-07-15 16:54:01 +0530

    Add files via upload

---
+++ beginner level/codekata33.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     15-07-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+n=int(raw_input())
+l=[int(x) for x in raw_input().split()]
+r=l[0]
+for i in range(len(l)-1):
+    if(l[i]>l[i+1]):
+         r=l[i+1]
+
+print r
