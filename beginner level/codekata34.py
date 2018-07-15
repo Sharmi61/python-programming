f07da952cbb401b6319f0463b9573704099552aa
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-07-15 17:10:05 +0530

    Add files via upload

---
+++ beginner level/codekata34.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     15-07-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+n=int(raw_input())
+l=[int(x) for x in raw_input().split()]
+temp=0
+for i in range(len(l)):
+    for j in range(len(l)):
+        if l[i]<l[j]:
+            temp=l[j]
+            l[j]=l[i]
+            l[i]=temp
+print l
+
+
