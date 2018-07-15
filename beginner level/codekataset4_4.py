6f650c164f48a58d6d9479a9af519afa7ca4eaac
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-07-15 20:12:51 +0530

    Add files via upload

---
+++ beginner level/codekataset4_4.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     15-07-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#------------------------------------------------------------------------------
+l=[str(x) for x in raw_input().split(".")]
+print len(l)
