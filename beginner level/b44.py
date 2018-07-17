22edbd29a9d505ba3221cbe9e741ca58e2e7a882
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-07-17 23:31:15 +0530

    Add files via upload

---
+++ beginner level/b44.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     17-07-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a=int(raw_input())
+if a>=1 and a<=10:
+    print "yes"
+else:
+    print "no"
