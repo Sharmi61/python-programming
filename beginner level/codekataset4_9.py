d2cef707a0b8f401d83f60a932794ce479e086e3
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-07-17 23:15:10 +0530

    Add files via upload

---
+++ beginner level/codekataset4_9.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     17-07-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+l=[int(x) for x in raw_input().split()]
+print max(l)
