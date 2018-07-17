2c76c2cda657d55d2389fe42b4a5c0ed881c2d3b
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-07-17 23:21:57 +0530

    Add files via upload

---
+++ beginner level/bs41.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     17-07-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+s,a=raw_input().split()
+s=str(s)
+a=int(a)
+for i in range(a):
+    print s
