d4066a16d028d8302347ec35f8192b3e40968dfd
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-07-24 22:08:37 +0530

    Add files via upload

---
+++ player level/p35.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     24-07-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+s=raw_input()
+l=[str(x) for x in raw_input().split()]
+for i in range(len(l)):
+    for j in range(len(l)):
+        if len(l[i])>len(l[j]):
+            l.append(l[i])
+            l.remove(l[i])
+for x in l:
+    print x,
