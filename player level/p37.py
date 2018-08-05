e11cc67d6d5adf53f6c50e05c7194aaae68dbadd
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-08-05 06:02:24 -0700

    Add files via upload

---
+++ player level/p37.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     05-08-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+r=str(raw_input())
+l=list(r)
+for i in range(len(l)):
+    if l[i].isupper():
+        l[i]=l[i].lower()
+    elif l[i].islower() :
+        l[i]=l[i].upper()
+print l
