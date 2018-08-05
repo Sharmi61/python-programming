cf77001341824d3a79c05cd27a7f9ec06fcb3f85
Sharmi61 <36623527+Sharmi61@users.noreply.github.com>
2018-08-05 05:48:25 -0700

    Add files via upload

---
+++ player level/p36.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      sharmi
+#
+# Created:     05-08-2018
+# Copyright:   (c) sharmi 2018
+# Licence:     <your licence>
+#-----------------------------------------------------------------------------
+r=str(raw_input())
+l=list(r)
+s=len(l)-1
+for i in range(s):
+    print l[i]
+    if l[i]==" ":
+        l.remove(l[i])
+        if i!=s-1:
+            i=i+1
+            s=s-1
+        else:
+            break
+
+s1="".join(l)
+print s1
